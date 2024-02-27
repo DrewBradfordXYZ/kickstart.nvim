229eea8a444a4bfa1decc18d2807e58e936023a7
Drew Bradford <drewbradfordxyz@gmail.com>
2024-02-26 21:24:41 -0500

    move files to custom folder

--- init.lua
+++ init.lua
@@ -812,5 +812,5 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-require 'keymaps'
-require 'options'
+require 'lua/custom/keymaps'
+require 'lua/custom/options'
