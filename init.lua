746dd70b2bb6dc8928c7edc00c1cf1ea836a3260
Drew Bradford <drewbradfordxyz@gmail.com>
2024-03-06 15:07:45 -0500

    Indentation set to 4
    I accidentally set these options in the init.lua

--- init.lua
+++ init.lua
@@ -770,23 +770,6 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     config = function()
-      -- Set the number of spaces a tab character represents in the file.
-      -- This affects movement in normal mode and the appearance of tabs.
-      vim.o.tabstop = 4
-
-      -- Set the number of spaces for each step of (auto)indentation.
-      -- Also, control the behavior of the Tab key in insert mode and visual mode.
-      -- This works in conjunction with 'expandtab'.
-      vim.o.shiftwidth = 4
-
-      -- Set the number of spaces a <Tab> in the file counts for when performing
-      -- editing operations like inserting or deleting text.
-      -- This provides flexibility for consistent indentation in mixed indentation styles.
-      vim.o.softtabstop = 4
-
-      -- Expand <Tab> to spaces, ensuring consistent indentation.
-      -- This works in conjunction with 'shiftwidth'.
-      vim.o.expandtab = true
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
       ---@diagnostic disable-next-line: missing-fields
--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,10 +1,23 @@
 -- Set relative numbered lines
 vim.o.relativenumber = true
 
--- Indentation
-vim.o.expandtab = true -- Use spaces instead of tabs
-vim.o.softtabstop = 4 -- Use this amount of spaces for indentation
-vim.o.shiftwidth = 4 -- Number of spaces to use for auto-indentation. For example, when using >> or <<
+-- Set the number of spaces a tab character represents in the file.
+-- This affects movement in normal mode and the appearance of tabs.
+vim.o.tabstop = 4
+
+-- Set the number of spaces for each step of (auto)indentation.
+-- Also, control the behavior of the Tab key in insert mode and visual mode.
+-- This works in conjunction with 'expandtab'.
+vim.o.shiftwidth = 4
+
+-- Set the number of spaces a <Tab> in the file counts for when performing
+-- editing operations like inserting or deleting text.
+-- This provides flexibility for consistent indentation in mixed indentation styles.
+vim.o.softtabstop = 4
+
+-- Expand <Tab> to spaces, ensuring consistent indentation.
+-- This works in conjunction with 'shiftwidth'.
+vim.o.expandtab = true
 
 -- Set side-scroll offset to 8 columns
 vim.o.sidescrolloff = 8
