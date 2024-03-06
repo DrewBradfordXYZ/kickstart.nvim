1e2a12ec9f25c8a25a99e2905395dc9966aec034
Drew Bradford <drewbradfordxyz@gmail.com>
2024-03-06 14:57:30 -0500

    Window split and navigation keybinds

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,6 +1,10 @@
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
+--  See `:help wincmd` for a list of all window commands
+keymap('n', '<C-j>', '<C-w>s', { desc = 'Split window horizontally' })
+keymap('n', '<C-l>', '<C-w>v', { desc = 'Split window vertically' })
+
 -- System clipboard
 vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p', { noremap = true, silent = true, desc = 'System Clipboard: Paste after' })
 vim.keymap.set({ 'n', 'v' }, '<leader>P', '"+P', { noremap = true, silent = true, desc = 'System Clipboard: Paste before' })
@@ -23,5 +27,11 @@ keymap('v', '<', '<gv', opts)
 keymap('v', '>', '>gv', opts)
 
 -- Navigate to the first and last characters of a line
-keymap({ 'n', 'o', 'x' }, '<s-h>', '^', opts)
-keymap({ 'n', 'o', 'x' }, '<s-l>', 'g_', opts)
+-- keymap({ 'n', 'o', 'x' }, '<s-h>', '^', opts)
+-- keymap({ 'n', 'o', 'x' }, '<s-l>', 'g_', opts)
+
+-- Resize split horizontally and vertically
+keymap('n', '<C-Up>', ':resize +2<CR>', { noremap = true, silent = true })
+keymap('n', '<C-Down>', ':resize -2<CR>', { noremap = true, silent = true })
+keymap('n', '<C-Left>', ':vertical resize -2<CR>', { noremap = true, silent = true })
+keymap('n', '<C-Right>', ':vertical resize +2<CR>', { noremap = true, silent = true })
