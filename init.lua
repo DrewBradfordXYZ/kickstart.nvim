376e9be0917dcc6140d9a11af8ec29276b5b0eeb
Drew Bradford <drewbradfordxyz@gmail.com>
2024-03-06 14:30:42 -0500

    add autopairs plugin

--- init.lua
+++ init.lua
@@ -807,10 +807,10 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information see: :help lazy.nvim-lazy.nvim-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-require 'lua/custom/keymaps'
-require 'lua/custom/options'
+require 'custom.keymaps'
+require 'custom.options'
